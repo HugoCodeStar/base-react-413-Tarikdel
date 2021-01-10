9b5322355059e6ca6937a3b3c80089bf1564f0bc
Admin <tarik@gmail.com>
2021-01-09 21:33:09 -0500

    affichage de trois citations aléatoires

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ var list = require('./quotes.json')
 // Pour voir le contenu des citations
 // console.log(list)
 
+function Citation(){
+    let len=list.quotes.length;
+    let i=Math.floor(Math.random() * len);
+    let j=Math.floor(Math.random() * len);
+    let k=Math.floor(Math.random() * len);
+    return(
+        <div>
+            <p>{list.quotes[i].quote}</p>
+            <p>{list.quotes[i].author}</p>
+            <p>{list.quotes[j].quote}</p>
+            <p>{list.quotes[j].author}</p>
+            <p>{list.quotes[k].quote}</p>
+            <p>{list.quotes[k].author}</p>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                Les citations vont ici.
+                <Citation/>
+
             </header>
         </div>
     );
